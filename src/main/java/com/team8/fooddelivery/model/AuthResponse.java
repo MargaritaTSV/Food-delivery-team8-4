fecd79ef7d9e60254682dd7783d14e51f4629938
nachyn ondar <ondar.nb@phystech.edu>
2025-11-27 15:05:11 -0800

    исправлен AuthResponse.java

--- src/main/java/com/team8/fooddelivery/model/AuthResponse.java
+++ src/main/java/com/team8/fooddelivery/model/AuthResponse.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:src/main/java/com/team8/fooddelivery/util/AuthResponse.java
-package com.team8.fooddelivery.util;
-========
 package com.team8.fooddelivery.model;
->>>>>>>> 03f38c28dfcdba7f46bf2708ff4cd0e64408a386:src/main/java/com/team8/fooddelivery/model/AuthResponse.java
 
 import com.team8.fooddelivery.model.client.ClientStatus;
 import lombok.AllArgsConstructor;
